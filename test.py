993e1996557d80ec0cc70f2eff8aa00c09e6a060
Nick <nickabbasi1@gmail.com>
2021-02-14 14:04:39 -0500

    added test fiele to find way to loop through final result set. need to add into main file

---
+++ test.py
@@ -0,0 +1,14 @@
+string = '<td valign="top"><b><a href="/sites/hist.phtml?station=BWI&amp;network=MD_ASOS&amp;mode=daily&amp;year=2021&amp;month=1&amp;day=01">01</a></b><br/>High: 39<br/>Low: 30<br/>Rain: 1.02<br/>Snow: 0<br/>Snow Depth: 0<br>Avg Wind: NE @ 6.1<br/>Gust:<br/> E @ 33<br/>(10:04 PM)<br>RH% Min/Max: 59-97<br>Feel Min/Max: 27 to 35</br></br></br></td>'
+
+split1 = string.split('<br')
+length = len(split1[1:])
+print(length)
+cell = 1
+while cell <= length:
+    if 'Gust' in split1[cell]:
+        cell+=1
+        print(split1[cell])
+    else:
+        print(split1[cell])
+
+    cell+=1
--- weather.py
+++ weather.py
@@ -12,8 +12,36 @@ for t in table:
     rows = t.findChildren('tr')
     for r in rows[2:]:
         cells = r.findChildren('td')
+
         for c in cells:
-            print(c)
+            #fin = c.find_all("bgcolor" != '#EEEEEE')
+            if 'href' in str(c):
+                #print(c)
+
+                day = c.find('a')
+                print(day.text)
+                vals = c.find_all('br')
+                for v in vals:
+                    print(v.text)
+                    print('\n')
+
+
+
+                print('*************')
+
+
+                #print(string.text)
+
+
+
+            #print(len(c))
+                #print('**********')
+#print(len(cells))
+
+        #    fin = c.findChildren('br')
+            #for f in fin:
+                #print(f)
+            #print(c)
 
     #print(rows[3:])
     #for r in rows:
