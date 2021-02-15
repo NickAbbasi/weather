e60bed8c0a3bcb8d6cf1394d8613cb2fe74177cf
Nick Abbasi <nabbasi@merrittproperties.com>
2021-02-15 16:06:27 -0500

    dumps results to dataframe. need to clean up wind collums as its still grabbing multiple conditions based on the @ sign

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
+from bs4 import BeautifulSoup as bs
 string = '<td valign="top"><b><a href="/sites/hist.phtml?station=BWI&amp;network=MD_ASOS&amp;mode=daily&amp;year=2021&amp;month=1&amp;day=01">01</a></b><br/>High: 39<br/>Low: 30<br/>Rain: 1.02<br/>Snow: 0<br/>Snow Depth: 0<br>Avg Wind: NE @ 6.1<br/>Gust:<br/> E @ 33<br/>(10:04 PM)<br>RH% Min/Max: 59-97<br>Feel Min/Max: 27 to 35</br></br></br></td>'
 
 split1 = string.split('<br')
 length = len(split1[1:])
 print(length)
+day = string.find('a')
+#day = day.text
+print(day)
 cell = 1
+
+
 while cell <= length:
     if 'Gust' in split1[cell]:
         cell+=1
--- weather.py
+++ weather.py
@@ -1,10 +1,25 @@
 import requests
 from bs4 import BeautifulSoup as bs
-
+import pandas as pd
+year ='2021'
+month = '1'
 url = 'https://mesonet.agron.iastate.edu/sites/hist.phtml?station=BWI&network=MD_ASOS&year=2021&month=1'
 
 page = requests.get(url)
-
+dict = {'day':[],
+        'month':[],
+        'year':[],
+        'High':[],
+        'Low':[],
+        'Rain':[],
+        'Snow':[],
+        'Snow Depth':[],
+        'Avg Wind':[],
+        'Wind Direction':[],
+        'Gust':[],
+        #'Time':[],
+        'RH Min/Max':[],
+        'Feel':[]}
 soup = bs(page.content, 'html.parser')
 #print(soup.text)
 table = soup.findChildren('table')
@@ -19,15 +34,71 @@ for t in table:
                 #print(c)
 
                 day = c.find('a')
-                print(day.text)
-                vals = c.find_all('br')
-                for v in vals:
-                    print(v.text)
-                    print('\n')
+                dict['day'].append(str(day.text))
+                dict['month'].append(str(month))
+                dict['year'].append(str(year))
+                split1 = str(c).split('<br')
+                length = len(split1[1:])
+                x = 1
+                if 'Gust' not in str(c):
+                    dict['Gust'].append('')
+                if 'High' not in str(c):
+                    dict['High'].append('')
+                if 'Low' not in str(c):
+                    dict['Low'].append('')
+                if 'Rain' not in str(c):
+                    dict['Rain'].append('')
+                if 'Snow:' not in str(c):
+                    dict['Snow'].append('')
+                if 'Snow Depth' not in str(c):
+                    dict['Snow Depth'].append( '')
+                if 'Avg Wind:' not in str(c):
+                    dict['Avg Wind'].append('')
+                if '@' not in str(c):
+                    dict['Wind Direction'].append( '')
+                if 'RH' not in str(c):
+                    dict['RH Min/Max'].append('')
+                if 'Feel' not in str(c):
+                    dict['Feel'].append('')
+
+                while x <= length:
+                    if 'Gust' in split1[x]:
+                        x+=1
+                        #print(split1[x])
+                        dict['Gust'].append(split1[x])
+                    else:
+                        if 'High' in split1[x]:
+                            dict['High'].append(split1[x])
+                        if 'Low' in split1[x]:
+                            dict['Low'].append(split1[x])
+                        if 'Rain' in split1[x]:
+                            dict['Rain'].append(split1[x])
+                        if 'Snow:' in split1[x]:
+                            dict['Snow'].append(split1[x])
+                        if 'Snow Depth' in split1[x]:
+                            dict['Snow Depth'].append(split1[x])
+                        if 'Avg Wind:' in split1[x]:
+                            dict['Avg Wind'].append(split1[x])
+                        if '@' in split1[x] and 'Avg Wind:' not in split1[x] :
+                            dict['Wind Direction'].append(split1[x])
+                        if 'RH' in split1[x]:
+                            dict['RH Min/Max'].append(split1[x])
+                        if 'Feel' in split1[x]:
+                            dict['Feel'].append(split1[x])
+                        #print(split1[x])
+
+                    x+=1
+                #vals = c.find_all('br')
+                #for v in vals:
+                #    print(v.text)
+                #    print('\n')
 
 
 
-                print('*************')
+                #print('*************')
+df = pd.DataFrame.from_dict(dict)
+df.to_excel('..\weather.xlsx')
+print(df)
 
 
                 #print(string.text)
